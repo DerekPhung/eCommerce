1c008a17785d338bc9989656d3de226252afb556
Sadiq <sssalewala@gmail.com>
2023-11-29 22:42:15 -0500

    added navbar component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { NavigationBarComponent } from './app/navigation-bar/navigation-bar.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
+bootstrapApplication(NavigationBarComponent, appConfig)
   .catch((err) => console.error(err));
