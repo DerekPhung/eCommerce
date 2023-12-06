6aa6408880e096692522d4b2df9a8ca9a43467a6
Sadiq <sssalewala@gmail.com>
2023-12-06 13:58:37 -0500

    adding to gitignore and how index.html file is laid out

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { NavigationBarComponent } from './app/navigation-bar/navigation-bar.component';
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-bootstrapApplication(NavigationBarComponent, appConfig)
   .catch((err) => console.error(err));
