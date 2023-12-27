fb880e7f68fe208b59bb873f5f84492a4972463b
Vi Nguyen <32149586+ViNguyen15@users.noreply.github.com>
2023-12-26 23:10:01 -0500

    Derek branch (#4)
    * first angular commit
    
    * finished sign up page and updated nav bar and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+
+import { SignupComponent } from './signup/signup.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationBarComponent,ProductPageComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavigationBarComponent,ProductPageComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+// import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: 'home', component: AppComponent },
+    { path: 'shop', component: ProductPageComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
