399f3c17bb5f8e04656b1a3478c2f82e927ad0c3
livingRoomFluff <fluffywin@gmail.com>
2023-12-26 20:28:16 -0500

    first angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavigationBarComponent,ProductPageComponent],
+  imports: [CommonModule, RouterOutlet, NavigationBarComponent,ProductPageComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
