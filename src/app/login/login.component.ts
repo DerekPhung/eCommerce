50a1df5a2caac34e662207581f4d7f601644c192
Vi Nguyen <32149586+ViNguyen15@users.noreply.github.com>
2023-12-28 23:21:17 -0500

    Home page (#5)
    * added home component
    
    * added cart component, home, login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
