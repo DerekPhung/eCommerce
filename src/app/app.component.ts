cabf79f923c29228a03c2f7381e9770bd6ab7d0f
livingRoomFluff <fluffywin@gmail.com>
2024-01-03 15:23:43 -0500

    Deric, Sadiq, Jerry changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { CartComponent } from './cart/cart.component';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavigationBarComponent, CartComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
